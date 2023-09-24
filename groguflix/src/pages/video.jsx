dac51330b9309a6c843a99bbd119c1076a5826ec
Noe <noe91.carranza@gmail.com>
2023-09-24 12:34:56 -0300

    modificando con will

--- groguflix/src/pages/video.jsx
+++ groguflix/src/pages/video.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { buscar } from "../API/api";
 import '../css/video.css';
+import PropTypes from "prop-types";
 
 
 const Video = ({ url }) => {
@@ -15,35 +16,37 @@ const Video = ({ url }) => {
 
     useEffect(() => {
         buscar(`/videos/${id}`, setVideos)
-    },[])
+    }, [])
 
     return (
 
-        <div style={{backgroundColor:'black'}}>
-            
-                <h2 style={{fontSize:'3rem'}}>{videos.nombre}</h2>
-                <h3>{videos.año}</h3>
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    padding: '20px',
-                    justifyItems: 'center',
-                    justifyContent: 'center',
-                    gap: '40px',
-                }}>
-                <h3 style={{ textAlign:'justify', width:'350px'}}>{videos.descripcion}</h3>
-            <img className="poster"src={`${videos.imagen}`} style={{ width: '350px'}}></img>
+        <div style={{ backgroundColor: 'black' }}>
+
+            <h2 style={{ fontSize: '3rem' }}>{videos.nombre}</h2>
+            <h3>{videos.año}</h3>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                padding: '20px',
+                justifyItems: 'center',
+                justifyContent: 'center',
+                gap: '40px',
+            }}>
+                <h3 style={{ textAlign: 'justify', width: '350px' }}>{videos.descripcion}</h3>
+                <img className="poster" src={`${videos.imagen}`} style={{ width: '350px' }}></img>
             </div>
-            <iframe 
-            width="853"
-            height="480"
-            src={`${videos.URL}`} 
-            allow='autoplay; accelerometer;clipboard-write;encrypted-media'
-            allowFullScreen>
+            <iframe
+                width="853"
+                height="480"
+                src={`${videos.URL}`}
+                allow='autoplay; accelerometer;clipboard-write;encrypted-media'
+                allowFullScreen>
             </iframe>
-            
         </div>
-    )
+    );
+    Video.propTypes = {
+        embedId: PropTypes.string.isRequired
+    };
 }
 
 export default Video
